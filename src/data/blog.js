344320467621b86e2f6612b683ee0c2ff2ad4514
Victor <victor.gitonga1@student.moringaschool.com>
2025-04-17 14:28:57 +0300

    comp props

--- src/data/blog.js
+++ src/data/blog.js
@@ -1,31 +1,30 @@
-import logo from "../assets/logo";
-
 const blogData = {
-  name: "Underreacted",
-  image: logo,
-  about: "A blog about learning React",
+  name: "Overreacted",
+  image: "https://via.placeholder.com/215",
+  about: "Personal blog by Dan Abramov. I explain with words and code.",
   posts: [
     {
       id: 1,
-      title: "Components 101",
-      date: "December 15, 2020",
-      preview: "Setting up the building blocks of your site",
-      minutes: 5,
+      title: "React is Not a Framework",
+      date: "July 7, 2020",
+      preview: "React lets you build user interfaces out of individual pieces...",
+      minutes: 11
     },
     {
       id: 2,
-      title: "React Data Flow",
-      date: "December 11, 2020",
-      preview: "Passing props is never passé",
-      minutes: 15,
+      title: "The WET Codebase",
+      date: "July 1, 2020",
+      preview: "Come waste your time with me...",
+      minutes: 5
     },
     {
       id: 3,
-      title: "Function vs Class Components",
-      preview: "React, meet OOJS.",
-      minutes: 47,
-    },
-  ],
+      title: "Goodbye, Clean Code",
+      date: "June 28, 2020",
+      preview: "Let clean code guide you. Then let it go.",
+      minutes: 47
+    }
+  ]
 };
 
 export default blogData;
